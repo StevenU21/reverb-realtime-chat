c842034de9e6f7ddb3a831ad19a5fad452dde8d0
StevenU21 <ulloadeifheltsteven@gmail.com>
2024-08-06 16:25:52 -0600

    chore: Add columns 'name' and 'slug' to conversations table migration

--- database/migrations/2024_08_06_222418_create_conversations_table.php
+++ database/migrations/2024_08_06_222418_create_conversations_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('conversations', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
     }
