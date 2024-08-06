58cae984fce6f95479adcf4024c8496721c366c8
StevenU21 <ulloadeifheltsteven@gmail.com>
2024-08-06 16:31:37 -0600

    chore: Update messages table migration to include foreign key constraints and indexes

--- database/migrations/2024_08_06_222904_create_messages_table.php
+++ database/migrations/2024_08_06_222904_create_messages_table.php
@@ -12,7 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('messages', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->text('content');
+            
+            $table->integer('conversation_id')->unsigned();
+            $table->foreign('conversation_id')->references('id')->on('conversations')->onDelete('cascade');
+
+            $table->integer('sender_id')->unsigned();
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('receiver_id')->unsigned();
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->index(['conversation_id', 'sender_id', 'receiver_id']);
             $table->timestamps();
         });
     }
